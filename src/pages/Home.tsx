fc2d4c6e7340abdb88f28ef2a1e4f926bfb60aa3
Puidor <c.adriano.silva@hotmail.com>
2021-06-26 12:47:59 -0300

    Light/Dark Mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useState } from "react";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
+import change from "../assets/images/change.svg";
 
 import "../styles/auth.scss";
 
@@ -11,10 +12,13 @@ import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
+import { useTheme } from "../hooks/useTheme";
+
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
   const [roomCode, setRoomCode] = useState("");
+  const { theme, toggleTheme } = useTheme();
 
   async function handleCreateRoom() {
     if (!user) {
@@ -46,7 +50,7 @@ export function Home() {
   }
 
   return (
-    <div id="page-auth">
+    <div id="page-auth" className={theme}>
       <aside>
         <img
           src={illustrationImg}
@@ -58,6 +62,12 @@ export function Home() {
 
       <main>
         <div className="main-content">
+          {/* Light/Dark Mode */}
+          <button onClick={toggleTheme} className="change-theme">
+            <img src={change} alt="Icone da troca de tema" />
+            Change Theme
+          </button>
+
           <img src={logoImg} alt="Letmeask" />
 
           <button onClick={handleCreateRoom} className="create-room">
